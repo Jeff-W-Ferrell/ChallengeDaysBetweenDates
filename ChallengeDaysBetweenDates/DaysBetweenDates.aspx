bb94298760e60e7c604baabcfd99561b21af66a4
Jeff Ferrell <jwferrell78@gmail.com>
2017-10-23 11:55:29 -0700

    All files on board.
    First commit lacked csproject, .aspx files, and .config files.

---
+++ ChallengeDaysBetweenDates/DaysBetweenDates.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DaysBetweenDates.aspx.cs" Inherits="ChallengeDaysBetweenDates.DaysBetweenDates" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        How many days have elapsed?<br />
+        <br />
+        Choose one date:<asp:Calendar ID="firstDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        <br />
+        Choose second date:<asp:Calendar ID="secondDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="timeSpanLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/DaysBetweenDates.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class DaysBetweenDates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            //firstDateCalendar.SelectedDate 
+            //secondDateCalendar.SelectedDate
+
+            TimeSpan daysBetweenDates = secondDateCalendar
+                .SelectedDate.Subtract(firstDateCalendar.SelectedDate);
+
+            if (firstDateCalendar.SelectedDate > secondDateCalendar.SelectedDate)
+                daysBetweenDates = firstDateCalendar
+                    .SelectedDate.Subtract(secondDateCalendar.SelectedDate);
+
+            timeSpanLabel.Text = "There are " + daysBetweenDates.TotalDays.ToString() + " days between those dates.";
+        }
+    }
+}
